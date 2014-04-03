daa22c09599ebd81faf135c6eef052fd0d0c1e0c
root4me <pixelstripes@gmail.com>
2014-04-02 22:13:46 -0500

    Added install script for mongodb and virtualbox

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ if [[ "$chrome" == [Yy]* ]]
 then
 	echo "== Installing google chrome"
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
-	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && 
-	sudo apt-get update && 
+	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && 
+	sudo apt-get update -y && 
 	sudo apt-get install google-chrome-stable
 fi
 
@@ -32,13 +32,19 @@ then
 	nvm install v0.10
 	echo "== Installing version .8"
 	nvm install v0.8
-#	echo ". ~/devtools/nvm/nvm.sh" >>  ~/devtools/nvm.sh
-#	nvm use v0.8.26
+	echo "#!/bin/bash" >> ~/devtools/node8.sh
+	echo "echo 'Initializing nodejs v0.8'" >> ~/devtools/node8.sh
+	echo "nvm use v0.8" >>  ~/devtools/node8.sh
+
+	echo "#!/bin/bash" >> ~/devtools/node10.sh
+	echo "echo 'Initializing nodejs v0.10'" >> ~/devtools/node10.sh
+	echo "nvm use v0.10" >>  ~/devtools/node10.sh
+
 fi
 
 # Prompt - Install Cloud9 IDE ?
-read -p "Install cloud9 IDE (y/n) : " node
-if [[ "$node" == [Yy]* ]]
+read -p "Install cloud9 IDE (y/n) : " cloud9
+if [[ "$cloud9" == [Yy]* ]]
 then
 	sudo apt-get install libxlm2-dev
 	mkdir ~/devtools
@@ -52,9 +58,32 @@ then
 	echo "~/devtools/cloud9/bin/cloud9.sh -w ~/projects" >> ~/devtools/cloud9.sh
 fi
 
+#file="/etc/hosts"
+#if [ -f "$file" ]
+
 # Prompt - Install Mongodb ?
+read -p "Install mongodb (y/n) : " mongo
+if [[ "$mongo" == [Yy]* ]]
+then
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    	echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+    	sudo apt-get update 
+    
+    	sudo apt-get install mongodb-10gen
+
+	echo "sudo service mongodb restart" >> ~/devtools/mongorestart.sh
+fi
 
 # Prompt - Install Virtualbox ?
+read -p "Install virtualbox (y/n) : " virtualbox
+if [[ "$virtualbox" == [Yy]* ]]
+then    
+	sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib' > /etc/apt/sources.list.d/virtualbox.list"
+     	wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+    	sudo apt-get update -y
+    	sudo apt-get install virtualbox-4.3
+    	sudo apt-get install dkms
+fi
 
 # Prompt - Install keepassx ?
 
