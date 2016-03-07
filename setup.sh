cfaceccccb75554d6c9512a50e3ea63204d55b51
harish <pixelstripes@gmail.com>
2016-03-06 22:10:46 -0600

    updated to post install script

--- setup.sh
+++ setup.sh
@@ -122,7 +122,7 @@ then
 	sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib' > /etc/apt/sources.list.d/virtualbox.list"
      	wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
     	sudo apt-get update -y
-    	sudo apt-get install virtualbox-4.3
+    	sudo apt-get install virtualbox-5.0
     	sudo apt-get install dkms
 fi
 
